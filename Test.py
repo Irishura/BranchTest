510560dbc44bd99ada66d893cce591d2d57d0db3
hdssh <hadassa.irishura@strathmore.edu>
2026-01-21 14:16:12 +0300

    updated test

---
+++ Test.py
@@ -0,0 +1,16 @@
+
+# Simple login system
+
+#login credentials
+USERNAME = "hadassa"
+PASSWORD = "1234"
+
+#inputs
+username = input("username : ")
+password = input("passwword :")
+
+if username == USERNAME and password == PASSWORD :
+    print ("Login Successful!")
+
+else :
+    print ("Login Incorrect! Wrong username or password. ")
